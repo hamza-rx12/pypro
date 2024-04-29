4a899f264c94bb654f8b90822c8f1bb0f689df5c
hamza-rx12 <127843196+hamza-rx12@users.noreply.github.com>
2024-04-29 22:23:32 +0100

    notebook tabs added

--- image.py
+++ image.py
@@ -53,6 +53,15 @@ class image:
                 self.rotate(filter[1])
             elif filter[0]=="blur":
                 self.blur(filter[1])
+            elif filter[0]=="brightness":
+                self.brightness(filter[1])
+            elif filter[0]=="adjust_red_saturation":
+                self.adjust_red_saturation(filter[1])
+            elif filter[0]=="adjust_green_saturation":
+                self.adjust_green_saturation(filter[1])
+            elif filter[0]=="adjust_blue_saturation":
+                self.adjust_blue_saturation(filter[1])
+
         elif len(filter)==3:
             if filter[0]=="resize":
                 self.resize(filter[1],filter[2])
@@ -137,4 +146,68 @@ class image:
         # Apply the inverted mask to the original image to make it negative
         self.npimage = cv2.bitwise_and(255 - self.npimage, 255 - self.npimage, mask=mask)
         # self.npimage = cv2.cvtColor(self.npimage, cv2.COLOR_GRAY2BGR)
+        self.photoimage = self.convert_cv_to_photoimage(self.npimage)
+
+    def brightness(self, value):
+        alpha = 1.5 + value / 100.0  # facteur d'échelle pour la luminosité
+        beta = -0.5  # pour le décalaagge de la luminosite
+        self.npimage = cv2.convertScaleAbs(self.npimage, alpha=alpha, beta=beta)
+        self.photoimage = self.convert_cv_to_photoimage(self.npimage)
+
+    def adjust_green_saturation(self, green_saturation_factor):
+        # Convertir l'image en espace de couleurs HSV
+        hsv = cv2.cvtColor(self.npimage, cv2.COLOR_BGR2HSV)
+
+        # Définir la plage de valeurs HSV pour la couleur verte
+        lower_green = np.array([30, 50, 50])
+        upper_green = np.array([90, 255, 255])
+
+        # Créer un masque pour les pixels verts dans l'image HSV
+        green_mask = cv2.inRange(hsv, lower_green, upper_green)
+
+        # Ajuster la saturation des pixels verts dans l'image HSV
+        hsv[:, :, 1][green_mask > 0] = np.clip(hsv[:, :, 1][green_mask > 0] * (green_saturation_factor / 100.0), 0, 255)
+
+        # Convertir l'image HSV modifiée en image BGR
+        self.npimage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        self.photoimage = self.convert_cv_to_photoimage(self.npimage)
+
+    def adjust_red_saturation(self, red_saturation_factor):
+        # Convertir l'image en espace de couleurs HSV
+        hsv = cv2.cvtColor(self.npimage, cv2.COLOR_BGR2HSV)
+
+        # Définir la plage de valeurs HSV pour la couleur rouge
+        lower_red1 = np.array([0, 50, 50])
+        upper_red1 = np.array([10, 255, 255])
+        red_mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+
+        lower_red2 = np.array([170, 50, 50])
+        upper_red2 = np.array([180, 255, 255])
+        red_mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+
+        red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+
+        # Ajuster la saturation des pixels rouges dans l'image HSV
+        hsv[:, :, 1][red_mask > 0] = np.clip(hsv[:, :, 1][red_mask > 0] * (red_saturation_factor / 100.0), 0, 255)
+
+        # Convertir l'image HSV modifiée en image BGR
+        self.npimage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        self.photoimage = self.convert_cv_to_photoimage(self.npimage)
+
+    def adjust_blue_saturation(self, blue_saturation_factor):
+        # Convertir l'image en espace de couleurs HSV
+        hsv = cv2.cvtColor(self.npimage, cv2.COLOR_BGR2HSV)
+
+        # Définir la plage de valeurs HSV pour la couleur bleue
+        lower_blue = np.array([90, 50, 50])
+        upper_blue = np.array([150, 255, 255])
+
+        # Créer un masque pour les pixels bleus dans l'image HSV
+        blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+        # Ajuster la saturation des pixels bleus dans l'image HSV
+        hsv[:, :, 1][blue_mask > 0] = np.clip(hsv[:, :, 1][blue_mask > 0] * (blue_saturation_factor / 100.0), 0, 255)
+
+        # Convertir l'image HSV modifiée en image BGR
+        self.npimage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
         self.photoimage = self.convert_cv_to_photoimage(self.npimage)
--- imagemaster.py
+++ imagemaster.py
@@ -1,5 +1,6 @@
 import image
 from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
 from customtkinter import *
 import os
@@ -11,20 +12,65 @@ from PIL import Image,ImageTk
 class GUI(Tk):
     def __init__(self):
         super().__init__()
+
+        ttk.Style().configure("TNotebook", background="#3e3e3e", borderwidth=0, relief="flat");
+        ttk.Style().map("TNotebook.Tab", background=[("selected", "#5e5e5e")], foreground=[("selected", "white")]);
+        ttk.Style().configure("TNotebook.Tab", background="#3e3e3e", foreground="white");
+
+        self.tabs = []
+        self.title("Image Master")
+        
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(expand=True, fill="both")
+        self.add_tab()
+        self.current_tab = self.tabs[self.notebook.index("current")]
+        self.menu_bar()
+
+    def add_tab(self):
+        new_tab = Tab(self.notebook)
+        self.notebook.add(new_tab, text="Tab {}".format(self.notebook.index("end")))
+        self.tabs.append(new_tab)
+        self.current_tab = self.tabs[self.notebook.index("current")]
+        print("current tab: ",self.notebook.index("current"))
+    def menu_bar(self):
+        # current_tab = self.tabs[self.notebook.index("current")]
+        menubar = Menu(self ,font=("monospace", 10),activebackground="gray", bg="#2e2e2e", fg="white", borderwidth=0, relief="flat")
+        menu_file = Menu(menubar,bg="#2e2e2e", fg="white", borderwidth=0, relief="flat", tearoff=0)
+        menu_file.add_command(label="New", activebackground="gray", command=self.add_tab)
+        menu_file.add_command(label="Open", activebackground="gray", command=self.importimage)
+        menu_file.add_command(label="Save", activebackground="gray", command=self.saveimage)
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit", command=exit)
+        menubar.add_cascade(label="File", menu=menu_file)
+        self.config(menu=menubar)
+
+    def importimage(self):
+        # self.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        # self.tabs[self.notebook.index("current")].importimage()
+        self.tabs[self.notebook.index("current")].importimage()
+    
+    def saveimage(self):
+        # self.tabs[self.notebook.index("current")].saveimage()
+        self.tabs[self.notebook.index("current")].saveimage()
+        
+
+
+class Tab(Frame):
+    def __init__(self,master=None):
+        super().__init__(master, bg="#2e2e2e")
         self.filename = None 
         self.lbl = None 
         self.im = None
-        self.title("Image Master")
         self.config(bg="#1e1e1e")
 
-        self.menu_bar()
+        
         
         self.frame1 = LabelFrame(self, text="Image: ", padx=10, pady=10, width=700, height=600, bg="#1e1e1e", fg="white", font=("monospace", 10))
         self.frame1.grid(row=0,column=0,padx=10,pady=10)
         self.frame1.pack_propagate(False)
 
-        self.frame2 = LabelFrame(self, text="Tools: ", padx=10, pady=10, width=700, height=300, bg="#1e1e1e", fg="white", font=("monospace", 10))
-        self.frame2.grid(row=1,column=0,padx=10,pady=10,columnspan=2)
+        frame2 = LabelFrame(self, text="Tools: ", padx=10, pady=10, width=700, height=300, bg="#1e1e1e", fg="white", font=("monospace", 10))
+        frame2.grid(row=1,column=0,padx=10,pady=10,columnspan=2)
 
         self.frame3 = LabelFrame(self, text="Bars: ", padx=10, pady=10, width=300, height=600, bg="#1e1e1e", fg="white", font=("monospace", 10), relief="groove")
         self.frame3.grid(row=0,column=1,padx=10,pady=10,sticky='nsew')
@@ -38,44 +84,71 @@ class GUI(Tk):
                                      variable=blur_intensity, 
                                      command=lambda x=blur_intensity.get(): self.on_slide("blur",x) if x==int(x) else None )
         self.blur_slider.grid(row=0,column=1)
-        self.bl.grid(row=0,column=0)
+        self.bl.grid(row=0,column=0, padx=(10, 0), pady=5, sticky='w')
 
+        brightness_intensity = DoubleVar()
+        brightness_intensity.set(0)
+        self.brightness_label = Label(self.frame3, text="Brightness:", bg="#1e1e1e", fg="white")
+        self.brightness_slider = CTkSlider(self.frame3, from_=-100, to=100, number_of_steps=10,
+                                       variable=brightness_intensity,
+                                       command=lambda x=brightness_intensity.get(): self.on_slide("brightness", x) if x==int(x) else None)
+        self.brightness_label.grid(row=1, column=0, padx=(10, 0), pady=5, sticky='w')
+        self.brightness_slider.grid(row=1, column=1)
 
+        red_saturation_factor = DoubleVar()
+        red_saturation_factor.set(100)
+        self.red_saturation_label = Label(self.frame3, text="Red Saturation:", bg="#1e1e1e", fg="white")
+        self.red_saturation_slider = CTkSlider(self.frame3, from_=0, to=200,
+                                               number_of_steps=10,
+                                               variable=red_saturation_factor,
+                                               command=lambda x=red_saturation_factor.get(): self.on_slide(
+                                                   "adjust_red_saturation", x) if x == int(x) else None)
+        self.red_saturation_label.grid(row=2, column=0, padx=(10, 0), pady=5, sticky='w')
+        self.red_saturation_slider.grid(row=2, column=1)
+
+        green_saturation_factor = DoubleVar()
+        green_saturation_factor.set(100)
+        self.green_saturation_label = Label(self.frame3, text="Green Saturation:", bg="#1e1e1e", fg="white")
+        self.green_saturation_slider = CTkSlider(self.frame3, from_=0, to=255,number_of_steps=10,
+                                                 variable=green_saturation_factor,
+                                                 command=lambda x=green_saturation_factor.get(): self.on_slide(
+                                                     "adjust_green_saturation", x))
+        self.green_saturation_label.grid(row=3, column=0, padx=(10, 0), pady=5, sticky='w')
+        self.green_saturation_slider.grid(row=3, column=1)
+
+        blue_saturation_factor = DoubleVar()
+        blue_saturation_factor.set(100)
+        self.blue_saturation_label = Label(self.frame3, text="Blue Saturation:", bg="#1e1e1e", fg="white")
+        self.blue_saturation_slider = CTkSlider(self.frame3, from_=0, to=200,
+                                                variable=blue_saturation_factor,
+                                                command=lambda x=blue_saturation_factor.get(): self.on_slide(
+                                                    "adjust_blue_saturation", x))
+        self.blue_saturation_label.grid(row=4, column=0, padx=(10, 0), pady=5, sticky='w')
+        self.blue_saturation_slider.grid(row=4, column=1)
         
         self.importIm = Button(self.frame1, text="Import Image", command=self.importimage, bg="#383838", fg="white", borderwidth=0, activebackground="gray")
         self.importIm.place(relx=0.5, rely=0.5, anchor="center")
 
         
         edge_detect_var=BooleanVar()
-        self.edge_detect=CTkSwitch(self.frame2,text="Edge detect", variable=edge_detect_var, command=lambda: self.on_click(("edge_detect",),edge_detect_var))
-        self.edge_detect.grid(row=0,column=1,padx=10,pady=10)
+        edge_detect=CTkSwitch(frame2,text="Edge detect", variable=edge_detect_var, command=lambda: self.on_click(("edge_detect",),edge_detect_var))
+        edge_detect.grid(row=0,column=1,padx=10,pady=10)
 
         sharpen_var=BooleanVar()
-        self.sharpen=CTkSwitch(self.frame2,text="Sharpen", variable=sharpen_var, command=lambda: self.on_click(("sharpen",),sharpen_var))
-        self.sharpen.grid(row=0,column=2,padx=10,pady=10)
+        sharpen=CTkSwitch(frame2,text="Sharpen", variable=sharpen_var, command=lambda: self.on_click(("sharpen",),sharpen_var))
+        sharpen.grid(row=0,column=2,padx=10,pady=10)
 
         grayscale_var=BooleanVar()
-        self.grayscale=CTkSwitch(self.frame2,text="Grayscale", variable=grayscale_var, command=lambda: self.on_click(("grayscale",),grayscale_var))
-        self.grayscale.grid(row=0,column=3,padx=10,pady=10)
+        grayscale=CTkSwitch(frame2,text="Grayscale", variable=grayscale_var, command=lambda: self.on_click(("grayscale",),grayscale_var))
+        grayscale.grid(row=0,column=3,padx=10,pady=10)
 
         emboss_var=BooleanVar()
-        self.emboss=CTkSwitch(self.frame2,text="Emboss", variable=emboss_var, command=lambda: self.on_click(("emboss",),emboss_var))
-        self.emboss.grid(row=0,column=4,padx=10,pady=10)
+        emboss=CTkSwitch(frame2,text="Emboss", variable=emboss_var, command=lambda: self.on_click(("emboss",),emboss_var))
+        emboss.grid(row=0,column=4,padx=10,pady=10)
 
         negative_var=BooleanVar()
-        self.negative=CTkSwitch(self.frame2,text="Negative", variable=negative_var, command=lambda: self.on_click(("negative",),negative_var))
-        self.negative.grid(row=0,column=5,padx=10,pady=10)
-
-    def menu_bar(self):
-        menubar = Menu(self ,font=("monospace", 10),activebackground="gray", bg="#2e2e2e", fg="white", borderwidth=0, relief="flat")
-        menu_file = Menu(menubar,bg="#2e2e2e", fg="white", borderwidth=0, relief="flat", tearoff=0)
-        menu_file.add_command(label="New", activebackground="gray", command=None)
-        menu_file.add_command(label="Open", activebackground="gray", command=self.importimage)
-        menu_file.add_command(label="Save", activebackground="gray", command=self.saveimage)
-        menu_file.add_separator()
-        menu_file.add_command(label="Exit", command=exit)
-        menubar.add_cascade(label="File", menu=menu_file)
-        self.config(menu=menubar)
+        negative=CTkSwitch(frame2,text="Negative", variable=negative_var, command=lambda: self.on_click(("negative",),negative_var))
+        negative.grid(row=0,column=5,padx=10,pady=10)
 
     def on_slide(self,*filter):
         self.im.applyFilter(filter)
@@ -93,7 +166,7 @@ class GUI(Tk):
 
     def importimage(self):
         self.filename = filedialog.askopenfilename(
-            initialdir=".",  
+            initialdir="./src/",  
             title="Select image file",
             filetypes=[("Image files", "*.png *.jpg *.jpeg *.bmp *.gif")]
         )
@@ -129,13 +202,12 @@ class GUI(Tk):
             # self.im.image.photoimage = self.im.image.photoimage.subsample(2,2)
         else:
             print("No label found")
-        
-    
 
 
 class imageProcessor:
-    filterList=[]
+    # filterList=[]
     def __init__(self,path):
+        self.filterList=[]
         self.path=path
         self.image=image.image(path)
 
